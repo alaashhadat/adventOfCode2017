49207b14069521af6a737c3579cda68f3c3270e5
Alaa shhadat <alaa.shhadat@spronq.com>
2019-12-02 08:59:43 +0100

    day3

---
+++ day3.js
@@ -0,0 +1,21 @@
+const upSquare = n => Math.ceil(Math.sqrt(n));
+
+const getLayer = n => {
+  const m = upSquare(n);
+  return Math.ceil((m - 1) / 2);
+};
+
+const N = 289326;
+
+const dist = n => {
+  if (n === 1) return 0;
+  const l = getLayer(n);
+  n -= (2 * l - 1) ** 2 + 1;
+  const layerLen = 2 * l;
+  const lower = layerLen * Math.floor(n / layerLen);
+  const upper = lower + layerLen - 1;
+  const mid = (lower + upper - 1) / 2;
+  return l + Math.abs(n - mid);
+};
+
+console.log(dist(N));
